773abaa747765208a29db676c1ebe4ed0abbbc0d
johnny <johnnysoto961@hotmail.com>
2020-08-29 23:57:22 -0500

    Checking repository

---
+++ SampleStore.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SampleStore.Models
+{
+    class Category
+    {
+        [key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Category Order")]
+        public int DisplayOrder { get; set; }
+    }
+}
