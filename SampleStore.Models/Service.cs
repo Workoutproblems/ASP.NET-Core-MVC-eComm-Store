1c86a1fe9c3962c0d60d529558ab2bed883c9c4e
johnny <johnnysoto961@hotmail.com>
2020-09-14 00:16:28 -0500

    Migration for Service and Frequency added.

--- SampleStore.Models/Service.cs
+++ SampleStore.Models/Service.cs
@@ -10,23 +10,30 @@ namespace SampleStore.Models
     {
         [key]
         public int Id { get; set; }
+
         [Required]
         [Display(Name = "Service Name")]
         public string Name { get; set; }
+
         [Required]
         public double Price { get; set; }
+
         [Display(Name = "Description")]
         public string LongDesc { get; set; }
+
         [DataType(DataType.ImageUrl)]
         [Display(Name = "Image")]
         public string ImageUlr { get; set; }
+
         [Required]
         public int CateggoryId { get; set; }
+
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
 
         [Required]
         public int FrequencyId{ get; set; }
+
         [ForeignKey("FrequencyId")]
         public Frequency Frequency { get; set; }
     }
